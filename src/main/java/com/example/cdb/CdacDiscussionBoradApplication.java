4541bf1b547ff4e403b47f3b8a6aea68da86699f
das-gluck <05gaurav03@gmail.com>
2024-12-12 21:53:45 +0530

    First Commit

---
+++ src/main/java/com/example/cdb/CdacDiscussionBoradApplication.java
@@ -0,0 +1,14 @@
+package com.example.cdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdacDiscussionBoradApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdacDiscussionBoradApplication.class, args);
+		System.out.println("Hello Everyone !!!");
+	}
+
+}
---
+++ src/test/java/com/example/cdb/CdacDiscussionBoradApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdacDiscussionBoradApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
